c944be50393b77f82f672272580b5c05d598b886
Stephen Grier <git@grier.org.uk>
2015-10-08 01:13:11 +0100

    Configure the rack passenger application + disable the WEBrick puppetmaster service.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,6 +1,7 @@
 # The default manifest. Used to bootstrap the puppet master nodes.
 
 $basedir = '/etc/puppet'
+$rack_app_basedir = '/usr/share/puppet/rack/puppetmasterd'
 
 # Install puppet-server + other pre-req packages.
 package {['puppet-server',
@@ -28,6 +29,31 @@ exec {'passenger-install-apache2-module':
   refreshonly => true,
 }
 
+# Install the rack application.
+file {['/usr/share/puppet/rack',
+        $rack_app_basedir,
+       "${rack_app_basedir}/public",
+       "${rack_app_basedir}/tmp" ] :
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+file { '/usr/share/puppet/rack/puppetmasterd/config.ru':
+  ensure  => link,
+  target  => '/usr/share/puppet/ext/rack/config.ru',
+  owner   => 'puppet',
+  group   => 'puppet',
+  require => File[$rack_app_basedir],
+}
+
+# Apache config the the puppet master VH.
+file { '/etc/httpd/conf.d/puppetmaster.conf':
+  ensure  => file,
+  content => template('/vagrant/puppet/templates/puppetmaster.conf.erb'),
+  require => Package['httpd'],
+}
+
 file {'/etc/puppet/puppet.conf':
   ensure => file,
   content => template('/vagrant/puppet/templates/puppet.conf.erb'),
@@ -51,10 +77,15 @@ file {["${basedir}/environments",
   mode  => '0755',
 }
 
+# The WEBrick puppetmaster process should be stopped.
 service {'puppetmaster':
+  ensure => stopped,
+  enable => false,
+}
+# Run apache + the rack application.
+service { 'httpd':
   ensure => running,
-  require => [File['/etc/puppet/puppet.conf'],
-              
-             ],
+  enable => true,
+  require => File['/etc/httpd/conf.d/puppetmaster.conf'],
 }
 
